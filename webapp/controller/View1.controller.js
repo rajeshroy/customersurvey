d33435d0fe0feadfe6554e8d7a6195115c7eecf5
rajesh roy <rajeshraj500@gmail.com>
2017-04-21 10:44:31 +0000

    [DEV] Initialization of project

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function(Controller) {
+	"use strict";
+
+	return Controller.extend("sap.cust.surveycustomersurvey1.controller.View1", {
+
+	});
+});
