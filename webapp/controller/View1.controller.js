134c517a1799916f713b9f78b20057978d7d8602
Shrutika Mandloi <mandloishrutika31392@gmail.com>
2017-04-26 07:18:00 +0000

    [DEV] View Layout design *View Layout design

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,9 +1,31 @@
 sap.ui.define([
-	"sap/ui/core/mvc/Controller"
-], function(Controller) {
+	"sap/ui/core/mvc/Controller",
+	"sap/ui/model/json/JSONModel"
+], function(Controller, JSONModel) {
 	"use strict";
 
 	return Controller.extend("sap.cust.surveycustomersurvey1.controller.View1", {
-
+		
+	onInit:function(){
+	/*	var mParameter, oModel, oJSONModel = new JSONModel();
+		oModel = this.getView().getModel();
+		this.getView().setModel(oJSONModel, "QuestionSet");
+		mParameter = {
+			context: null,
+			success: jQuery.proxy(this.fnSuccessQuestionSet, this),
+			error: jQuery.proxy(this.fnErrorQuestionSet, this)
+		};
+		oModel.read("/QuestionDetSet", mParameter);*/
+		
+	},
+/*	fnSuccessQuestionSet : function(oSuccessData){
+		this.getView().getModel("QuestionSet").setProperty("/", oSuccessData.results);
+	},*/
+      onSave : function(){
+      	
+      	
+      	
+      	
+      }
 	});
 });
